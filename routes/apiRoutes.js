fc4d9b420584b502ee5d30414cccc691fdf0467e
Mason-Shadrick <masonthemason@gmail.com>
2020-07-24 14:35:09 -0500

    api routes, index.js from public, and html routes all working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/getPost", function(req, res) {
-    db.createpost.findAll({}).then(function(dbExamples) {
+    db.createPost.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
   app.post("/api/post", function(req, res) {
-    db.createpost.create(req.body).then(function(dbExample) {
+    db.createPost.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/post/:id", function(req, res) {
+    db.createPost.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
