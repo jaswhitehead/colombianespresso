3094fb5b07b1f3f07381025da1fc5d5e731df707
Mason-Shadrick <masonthemason@gmail.com>
2020-07-24 15:43:39 -0500

    work on classifieds: handlebars, api routes, and index. NOT WORKING NEED TA

---
+++ models/models-classifieds.js
@@ -0,0 +1,24 @@
+//creating classifieds add
+module.exports = function (sequelize, DataTypes) {
+  var Classified = sequelize.define("Classified", {
+    //type of data and validation for each column in the table
+    listing_name: { 
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    listing_description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    listing_location:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    price:{
+      type: DataTypes.INTEGER,
+      min: 0
+    },
+    userID: DataTypes.INTEGER
+  });
+  return Classified;
+};
