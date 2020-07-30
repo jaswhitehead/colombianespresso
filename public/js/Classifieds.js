a51e7cdfb39ee9acca47ef20baf304025a0fcc0f
Mason-Shadrick <masonthemason@gmail.com>
2020-07-29 21:13:35 -0500

    login/signup both functional, signup redirects to login working

--- public/js/Classifieds.js
+++ public/js/Classifieds.js
@@ -1,107 +1,103 @@
+$(document).ready(function(){
+
 // Get references to page elements
 var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
+var $submitBtn = $("#submit-classifieds");
 var $exampleList = $("#example-list");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(addPosting) {
+// The APIclassifieds object contains methods for each kind of request we'll make
+var APIclassifieds = {
+  saveExampleClassifieds: function(addPosting) {
     return $.ajax({
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       type: "POST",
       url: "api/classifieds",
-      data: JSON.stringify(addPosting)
+      data: JSON.stringify(addPosting),
     });
   },
-  getExamples: function() {
+  getExamplesClassifieds: function() {
     return $.ajax({
       url: "api/getClassifieds",
-      type: "GET"
+      type: "GET",
     });
   },
-  deleteExample: function(id) {
+  deleteExampleClassifieds: function(id) {
     return $.ajax({
       url: "api/classifieds/" + id,
-      type: "DELETE"
+      type: "DELETE",
     });
-  }
+  },
 };
-
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
+var refreshExamplesClassifieds = function() {
+  APIclassifieds.getExamplesClassifieds().then(function(data) {
     var $textPost = data.map(function(example) {
       var $a = $("<a>")
         .text(example.listing_name)
         .attr("href", "/classifieds/" + example.id);
-
       var $p = $("<p>").text(example.listing_description);
       var $pTwo = $("<p>").text(example.listing_location);
       var $pThree = $("<p>").text(example.price);
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
-          "data-id": example.id
+          "data-id": example.id,
         })
         .append($a);
-        
-
       var $button = $("<button>")
         .addClass("btn btn-danger float-right delete")
         .text("ｘ");
-
-      $li.append($p, $pTwo, $pThree)
+      $li.append($p, $pTwo, $pThree);
       $li.append($button);
-
-
       return $li;
     });
-
     $exampleList.empty();
     $exampleList.append($textPost);
   });
 };
-
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmitClassifieds = function(event) {
   event.preventDefault();
-
   var addPost = {
-    listing_name: $("#add-name").val().trim(),
-    listing_description: $("#add-description").val().trim(),
-    listing_location: $("#add-location").val().trim(),
-    price: $("#add-price").val().trim(),
+    listing_name: $("#add-name")
+      .val()
+      .trim(),
+    listing_description: $("#add-description")
+      .val()
+      .trim(),
+    listing_location: $("#add-location")
+      .val()
+      .trim(),
+    price: $("#add-price")
+      .val()
+      .trim(),
   };
-
   if (!(addPost.listing_name && addPost.listing_description)) {
     alert("You must enter an example text and description!");
     return;
   }
-
-  API.saveExample(addPost).then(function() {
-    refreshExamples();
+  APIclassifieds.saveExampleClassifieds(addPost).then(function() {
+    refreshExamplesClassifieds();
   });
-
   $("#post-title").val("");
   $("#post-body").val("");
 };
-
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClickClassifieds = function() {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
+  APIclassifieds.deleteExampleClassifieds(idToDelete).then(function() {
+    refreshExamplesClassifieds();
   });
 };
-
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$submitBtn.on("click", handleFormSubmitClassifieds);
+$exampleList.on("click", ".delete", handleDeleteBtnClickClassifieds);
+
+
+});
