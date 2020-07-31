1047e33ec2b019c21522295fc675ae3c71f34ec9
Jason Whitehead <jaswhitehead@gmail.com>
2020-07-31 12:24:58 -0500

    More CSS and form implementation JS

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,7 +1,20 @@
+// require("dotenv").config();
+
+// // Import the API keys
+// var keys = require("./keys");
+
+
 var city = "Murfreesboro"
 
+// Attempt at making submit button work
+// var city = "";
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=2bb930dcd6a209c3048a37004c1534d6", function(data) {
+// $("#search_button").on("click", function(event){
+//     event.preventDefault();
+//     var city = $("#search-term").val();
+
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=", function(data) {
     console.log(data);
 
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
