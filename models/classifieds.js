6c26700a90667f385a053e66a975a491ce5ba485
Jason Whitehead <jaswhitehead@gmail.com>
2020-08-01 15:01:08 -0500

    changed classifieds UserID to USER_ID

--- models/classifieds.js
+++ models/classifieds.js
@@ -18,7 +18,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.INTEGER,
       min: 0
     },
-    userID: DataTypes.INTEGER
+    user_ID: DataTypes.INTEGER
   });
   return Classified;
 };
