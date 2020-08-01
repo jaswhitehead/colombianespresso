b1332aff88f6e437f5a3084babdf1a9e5951d1fb
Jason Whitehead <jaswhitehead@gmail.com>
2020-08-01 09:59:01 -0500

    Added Local News View, JS File and Module to Local News page / Still need Script help and Search Button tie in

--- public/js/weather.js
+++ public/js/weather.js
@@ -14,7 +14,9 @@ var city = "Murfreesboro"
 //     var city = $("#search-term").val();
 
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "", function(data) {
+
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=2bb930dcd6a209c3048a37004c1534d6", function(data) {
     console.log(data);
 
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
@@ -34,6 +36,19 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "", funct
     $('.windSpeed').append(windSpeed + ' mph');
     $('.humidity').append(humidity + '%');
 
-    console.log(description);
+    // console.log(description);
 
 });
+
+//Another idea
+// function searchWeather(){
+
+//     var search =$("#search-term").val();
+    
+//     var base_url = "http://api.openweathermap.org/data/2.5/weather?q=" + search
+//     var path = "&units=imperial&APPID=2bb930dcd6a209c3048a37004c1534d6"
+//     $.ajax({
+//       url: base_url + path,
+//       dataType: 'json',
+//       success: function(data) {
+//           console.log(data)
